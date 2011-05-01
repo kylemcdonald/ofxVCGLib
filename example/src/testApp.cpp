a7ac00dc627470f9649262babef1033d96279a95
Kyle McDonald <kyle@kylemcdonald.net>
2011-05-01 17:00:02 -0400

    minimized the code so it's easier to see the essentials

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -2,33 +2,17 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
+	ofSetVerticalSync(true);
 	
-	hasJoined = false;
-	
-	if(modelLoader1.loadModel("duck_triangulate.dae", true)) {
-		mesh1 = modelLoader1.getMesh(0);
-		
+	if(modelLoader.loadModel("duck_triangulate.dae", true)) {
+		mesh = modelLoader.getMesh(0);
 	} else {
-		cout << " model 1 can't load? " << endl;
+		cout << "model failed to load" << endl;
 	}
-	
-	if(modelLoader2.loadModel("soccer.DAE", true)) {
-		mesh2 = modelLoader2.getMesh(0);
-	} else {
-		cout << " model 2 can't load? " << endl;
-	}
-	
+
 	glShadeModel(GL_SMOOTH);
 	light.setup();
 	ofEnableSeparateSpecularLight();
-	
-	int i = 0;
-	while( i < 1000 )
-	{
-		in.push_back(ofVec3f(ofRandom(100), ofRandom(100), ofRandom(100)));
-		i++;
-	}
-	
 }
 
 //--------------------------------------------------------------
@@ -36,27 +20,27 @@ void testApp::update(){
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-	
+void testApp::draw(){	
 	ofBackground(50, 50, 50, 0);
-	ofSetColor(255, 255, 255, 255);
+	ofSetColor(255);
+	
+	cam.begin();
+	glEnable(GL_DEPTH_TEST);
 	
 	ofPushMatrix();
-	ofTranslate(300, 400, 0);
-	ofRotate(180, 1.f, 0, 0);
-	mesh1.drawFaces();//mesh1.drawWireframe();
-	ofRotate(180, -1.f, 0, 0);
-	ofTranslate(200, 0, 0);
-	mesh2.drawFaces();
+	mesh.drawFaces();
 	
-	if(hasJoined)
-	{
+	/*
+	if(hasJoined) {
 		ofTranslate(200, 0, 0);
 		joined->drawFaces();
 	}
+	*/
 	
 	ofPopMatrix();
 	
+	glDisable(GL_DEPTH_TEST);
+	cam.end();
 }
 
 //--------------------------------------------------------------
@@ -65,46 +49,5 @@ void testApp::keyPressed(int key){
 	//joined = ofxVCG::joinMeshes(&mesh1, &mesh2);
 	//hasJoined = true;
 	
-	ofxVCG::cleanCloudPoints(&in, &out, 3.f);
-	
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-	
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-	
+	//ofxVCG::cleanCloudPoints(&mesh.getVertices(), &out, 3.f);
 }
--- example/src/testApp.h
+++ example/src/testApp.h
@@ -5,33 +5,20 @@
 #include "ofxVCGLib.h"
 #include "ofxAssimpModelLoader.h"
 
-//#include "ofxVCGLibDefinitions.h"
-
 class testApp : public ofBaseApp{
-	
 public:
 	void setup();
 	void update();
 	void draw();
 	
-	void keyPressed  (int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
+	void keyPressed(int key);
 	
-	ofMesh mesh1, mesh2;
-	ofxAssimpModelLoader modelLoader1, modelLoader2;
+	ofMesh mesh;
+	ofxAssimpModelLoader modelLoader;
 	ofLight	light;
-	
-	ofMesh *joined;
-	bool hasJoined;
 	
 	vector<ofVec3f> in;
 	vector<ofVec3f> out;
 	
+	ofEasyCam cam;
 };
